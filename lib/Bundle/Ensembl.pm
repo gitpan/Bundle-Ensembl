2fea632625171191c1647b85f76862156210b892
Ahamarshan JN <gene@gpse.org>
2007-07-03 11:54:04 +0000

    Import of ASHGENE/Bundle-Ensembl-0.03 from CPAN.
    gitpan-cpan-distribution: Bundle-Ensembl
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ASHGENE/Bundle-Ensembl-0.03.tar.gz
    gitpan-cpan-author:       ASHGENE
    gitpan-cpan-maturity:     released

--- lib/Bundle/Ensembl.pm
+++ lib/Bundle/Ensembl.pm
@@ -26,7 +26,7 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
 
 # Preloaded methods go here.
@@ -39,49 +39,352 @@ __END__
 
 =head1 NAME
 
-Bundle::Ensembl - Perl extension for blah blah blah
+Bundle::Ensembl - Bundle for installing Ensembl Perl Modules 
+(Built from dependencies of ENSEMBL_45 VERSION)
 
 =head1 SYNOPSIS
 
-  use Bundle::Ensembl;
-  blah blah blah
+C<perl -MCPAN -e 'install Bundle::Ensembl'>
+
+This is almost in a test, But works fine, Just to make things easier email: gene@gpse.org.
+
+=head1 CONTENTS
+
+CGI
+
+CGI::Ajax
+
+CGI::Application
+
+CGI::ArgChecker
+
+CGI::Authent
+
+CGI::Base
+
+CGI::BasePlus
+
+CGI::CList
+
+CGI::Cache
+
+CGI::Carp::DebugScreen
+
+CGI::ContactForm
+
+CGI::DBTables
+
+CGI::Debug
+
+CGI::Deurl
+
+CGI::Echo
+
+CGI::Enurl
+
+CGI::Ex
+
+CGI::Explorer
+
+CGI::FormBuilder
+
+CGI::FormFactory
+
+CGI::FormMagick
+
+CGI::FormManager
+
+CGI::Formalware
+
+CGI::Getopt
+
+CGI::Imagemap
+
+CGI::Lite
+
+CGI::LogCarp
+
+CGI::MiniSvr
+
+CGI::Minimal
+
+CGI::MultiValuedHash
+
+CGI::MxScreen
+
+CGI::NoPoison
+
+CGI::Out
+
+CGI::Panel
+
+CGI::PathInfo
+
+CGI::Persistent
+
+CGI::Portable
+
+CGI::Query
+
+CGI::QuickForm
+
+CGI::Request
+
+CGI::Response
+
+CGI::SSI
+
+CGI::SSI_Parser
+
+CGI::Screen
+
+CGI::SecureState
+
+CGI::Session
+
+CGI::Session::DB2
+
+CGI::Session::Encrypted
+
+CGI::SimpleCache
+
+CGI::SpeedyCGI
+
+CGI::State
+
+CGI::Test
+
+CGI::URI2param
+
+CGI::Untaint::Filenames
+
+CGI::UploadEasy
+
+CGI::Validate
+
+CGI::WML
+
+CGI::WebApp
+
+CGI::XML
+
+CGI::XMLForm
+
+CGI_Lite
+
+Class::DBI
+
+Class::DBI::DB2
+
+Class::DBI::Loader::DB2
+
+Class::DBI::Oracle
+
+Class::DBI::Plugin::CountSearch
+
+Class::DBI::Plugin::DeepAbstractSearch
+
+Class::DBI::Plugin::DeepAbstractSearchPager
+
+Class::DBI::Plugin::Pager
+
+Class::DBI::Storable
+
+Class::DBI::Template
+
+Class::DBI::Test::TempDB
+
+Class::DBI::mysql
+
+Class::Data::Reloadable
+
+Class::DataStore
+
+Class::Date
+
+Bundle::DBI
+
+DBD::mSQL
+
+DBD::mysql
+
+DBD::mysqlPP
+
+DB_File::DB_Database
+
+DB_File::Lock
+
+Compress::Zlib
+
+Compress::Raw::Zlib
+
+DBD::mysql
+
+GD
+
+GD::Simple
+
+GD::Polyline
+
+GD::Image
+
+GD::Polygon
+
+GD::SVG
+
+GD::Convert
+
+GD
+
+GD::Barcode
+
+GD::Barcode::Code93
+
+GD::Gauge
+
+GD::Graph
+
+GD::Graph3d
+
+GD::Image::AnimatedGif
+
+GD::Image::CopyIFS
+
+GD::Image::Orientation
+
+GD::Image::Thumbnail
+
+GD::SecurityImage
+
+GD::Text
+
+GDS2
+
+GIFgraph
+
+GISI
+
+GISI::MIFMID
+
+Template::Plugin::GD
+
+Bundle::ParallelUA
+
+Data::UUID
+
+Digest::MD5
+
+Storable
+
+LWP
+
+LWP
+
+LWP::Conn
+
+LWP::MediaTypes
+
+LWP::Parallel
+
+LWP::Protocol
+
+LWP::Protocol::http::SocketUnix
+
+LWP::RobotUA
+
+LWP::Simple
+
+LWP::UA
+
+LWP::UserAgent
+
+LWPx::ParanoidAgent
+
+SOAP::Lite
+
+XML::Parser
+
+XML::Simple
+
+Parse::RecDescent
+
+PDF::API2
+
+Spreadsheet::WriteExcel
+
+OLE::Storage_Lite
+
+Time::HiRes
+
+HTML::Template
+
+File::Temp
+
+Mail::Mailer
+
+Math::Bezier
+
+IO::String
+
+Image::Size
+
+Cwd   	 
+
+File::Spec 	
+
+File::Spec::Cygwin 	
+
+File::Spec::Epoc 	
+
+File::Spec::Functions 	
+
+File::Spec::Unix 	
+
+Perl::Version
+
+version
+
+DB_File
+
+CGI::Ajax
+
+CGI::Session
+
+Class::Accessor
+
+Class::Data::Inheritable
+
+Class::Std::Utils
+
+Class::Std
+
+Devel::StackTrace
+
+Bio::Das::Lite
+
+Exception::Class
 
 =head1 DESCRIPTION
 
-Stub documentation for Bundle::Ensembl, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
+A Bundle of Modules related to Ensembl Genome Browser Installation (Ensembl V45). 
+If there are any modules that needs to be installed please email me at 
+gene@gpse.org
 
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+Please see the README file and Run the ens_modtest.pl
 
 =head1 AUTHOR
 
-root, E<lt>root@suse.deE<gt>
+Ahamarshan.J.N @www.gpse.org @www.mellorlab.org
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by root
+Copyright (C) 2007-2009 by Ahamarshan.J.N @www.gpse.org @www.mellorlab.org
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
